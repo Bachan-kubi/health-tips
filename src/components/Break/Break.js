d8818fb6b62fa68c8aa03b95abae90775738623c
bachan kubi <felixghagra@gmail.com>
2022-09-29 15:15:00 +0600

    break time fixed

--- src/components/Break/Break.js
+++ src/components/Break/Break.js
@@ -2,23 +2,20 @@ import React, { useState } from 'react';
 import './Break.css'
 
 const Break = () => {
-    const [breakTime, setBreakTime] = useState()
-    const handleBreak = ()=>{
-        setBreakTime()
-    }
+    const [breakTime, setBreakTime] = useState();
     return (
         <div>
             <div>
             <h3>Add a Break</h3>
             <div className="break">
-                <button onClick={(e)=>handleBreak(10)}  className="break-items">10s</button>
-                <button className="break-items">20s</button>
-                <button className="break-items">30s</button>
-                <button className="break-items">40s</button>
-                <button className="break-items">50s</button>
+                <button onClick={()=>setBreakTime(10)}  className="break-items">10s</button>
+                <button onClick={()=>setBreakTime(20)} className="break-items">20s</button>
+                <button onClick={()=>setBreakTime(30)} className="break-items">30s</button>
+                <button onClick={()=>setBreakTime(40)} className="break-items">40s</button>
+                <button onClick={()=>setBreakTime(50)} className="break-items">50s</button>
             </div>
             <div>
-                <h4 className="details">Break Time:{breakTime} </h4>
+                <h4 className="details">Break Time: {breakTime} </h4>
             </div>
         </div>
         </div>
