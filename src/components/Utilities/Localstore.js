1ceac619a9b6f3a00c58265c143ee03f10beea5b
bachan kubi <felixghagra@gmail.com>
2022-09-29 22:01:36 +0600

    final commit

--- src/components/Utilities/Localstore.js
+++ src/components/Utilities/Localstore.js
@@ -1,26 +1,24 @@
-// use local storage to manage cart data
-const addToLocalStorage = breakTime => {
+// use local storage to store data
+const addToStorage = breakTime => {
     let breakTimeObj = {};
   
     breakTimeObj = breakTime;
-  
+    // console.log(breakTimeObj);
     localStorage.setItem('breakTimeObj', JSON.stringify(breakTimeObj));
   };
   
   const getStoredObj = () => {
     let breakTimeObj = 0;
   
-    //get the shopping cart from local storage
+    //get the data from local storage
     const storedbreakTimeObj = localStorage.getItem('breakTimeObj');
   
     if (storedbreakTimeObj) {
       breakTimeObj = JSON.parse(storedbreakTimeObj);
     }
   
-    // console.log('inside controller:');
-    // console.log(typeof breakTimeObj);
   
     return breakTimeObj;
   };
   
-  export { addToLocalStorage, getStoredObj };
+  export { addToStorage, getStoredObj };
